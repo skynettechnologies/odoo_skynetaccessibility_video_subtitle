99b0150e8ea71518ece95fd07191bcf52cbb2708
Skynet Technologies <developer3@skynettechnologies.com>
2026-08-14 18:31:12 +0530

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'SkynetAccessibility Video Subtitle',
+    'summary': 'Publish Captioned Video Content with AI-Powered Subtitles',
+    'description': "",
+    'version': '1.0.0',
+    'category': 'Website',
+    'author': 'Skynet Technologies USA LLC',
+    'website': 'https://www.skynettechnologies.com',
+    'support': 'hello@skynettechnologies.com',
+    'license': 'LGPL-3',
+    'depends': ['base', 'web', 'website'],
+    'data': [
+        'data/ir_config_parameter_data.xml',
+        'templates/website_widget_template.xml',
+        'website/dashboard_page.xml',
+        'views/video_subtitle_menu.xml',
+    ],
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
